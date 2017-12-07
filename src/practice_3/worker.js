507180101e31c5d48f87dd6c487b457c0405e09e
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-07 19:06:39 +0800

    Update worker.js

--- src/practice_3/worker.js
+++ src/practice_3/worker.js
@@ -1,1 +1,13 @@
+import person from "../../practice_2/person.js";
+export class Worker extends Person {
+  
+  constructor(name, age) {
+    super(name, age)
+  }
+  
+  introduce() {
+    return "I am a Worker. I have a job.";
+  }
+}
 
+module.exports = Worker;
