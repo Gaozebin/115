f9e3240064e37cfc8babf547a872bda053ff9230
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-07 21:03:30 +0800

    Update teacher.js

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,19 @@
+import Person from "../../src/practice_2/person.js";
+export class Teacher extends Person {
+  
+  constructor(name, age, num) {
+    super(name, age);
+    this.num = num;
+  }
+  
+  
+  introduce() {
+    if(this.num != undefined){
+      return super.introduce() + " I am a Teacher. I teach Class "+ this.num + ".";
+    }else{
+    return super.introduce() + " I am a Teacher. I teach No Class.";
+     }
+ }
+}
 
+module.exports = Teacher;
