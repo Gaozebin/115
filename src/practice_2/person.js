f570893fb3812cf0236b3b0ac0f00589e6e9b5b8
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-07 18:32:08 +0800

    Update person.js

--- src/practice_2/person.js
+++ src/practice_2/person.js
@@ -1,1 +1,13 @@
+export class person{
 
+   constructor(name, age){
+      this.name = name;
+          this.age = age;
+}
+
+    introduce(){
+      return "My name is"+ this.name + ". I am "+this.age +" years old.";
+  }
+}
+
+ module.exports = Person；
