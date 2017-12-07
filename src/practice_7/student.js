d13fcd2e86d1aa1aadfaf7eb9d63ec3bd6d9fd08
index.html <32197390+Gaozebin@users.noreply.github.com>
2017-12-07 21:44:45 +0800

    Update student.js

--- src/practice_7/student.js
+++ src/practice_7/student.js
@@ -1,1 +1,14 @@
+import Person from "../../src/practice_2/persomn.js";
+export class Student extends person {
 
+   constructor(name,age, num) {
+    super(name, age)
+      this.num = num;
+   }
+  
+  introduce() {
+    return super.introduce() + " I am a Student. I am at Class "+ this.num.number +".";
+  }
+}
+
+module.exports = Student;
